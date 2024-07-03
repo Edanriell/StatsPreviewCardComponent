c63c28965b4317c8bf549d82d16777b4cae82ec0
Edanriell <Edanriell@gmail.com>
2024-07-03 23:34:42 +0300

    finished config

---
+++ Client/src/pages/main/main-page.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "main-page",
+	standalone: true,
+	imports: [],
+	templateUrl: "./main-page.html"
+})
+export class MainPage {}
---
+++ Client/src/widgets/stats-preview/stats-preview.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "stats-preview",
+	standalone: true,
+	imports: [],
+	templateUrl: "./stats-preview.html",
+	styleUrl: "./stats-preview.less"
+})
+export class StatsPreview {}
